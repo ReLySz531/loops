57aa4d91347d864c91a55b6e100c2e3e333e53ac
ReLySz531 <ReLySz531@yahoo.com>
2023-02-10 08:38:45 -0500

    commit

---
+++ loops.js
@@ -0,0 +1,17 @@
+for(let i= 1000; i >= 0; i = i - 2) {
+    console.log(i);
+}
+for(let i = 0; i <= 10000; i++) {
+    if (i == "2500") alert("A quarter of the way there!");
+    else if (i == "5000") alert("Halfway there!");
+    else if (i == "10000") alert("The loop is done!");
+}
+const favourite = ["Naruto", "My Hero Academia", "The Walking Dead", "Yugioh", "Destination Truth"]
+console.log(favourite)
+for(let i=0; i < favourite.length; i++) {
+    console.log("My #1 favorite tv show is Naruto")
+    console.log("My #2 favorite tv show is My Hero Academia")
+    console.log("My #3 favorite tv show is The Walking Dead")
+    console.log("My #4 favorite tv show is Yu-Gi-Oh")
+    console.log("My #5 favorite tv show is Destination Truth")
+}
